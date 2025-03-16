2b1bce7858d2a98be9253b1a1631ec98f0f09e05
Jon <87449181+Caraveo@users.noreply.github.com>
2025-03-15 21:36:17 -0600

    fix: update static file serving configuration for proper image loading

--- artist-portfolio/src/server.js
+++ artist-portfolio/src/server.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 });
 
 // Serve static files from the public directory
-app.use('/public', express.static(path.join(__dirname, '../public'), {
+app.use(express.static(path.join(__dirname, '../public'), {
   fallthrough: true,
   index: false
 }));
